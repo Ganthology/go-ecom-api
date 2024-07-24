a446ac7d0b71aee3b70a1784cab38c8a0e1ce993
Ray Gan <ray.ganbk@gmail.com>
2024-07-25 00:26:53 +0800

    feat: add migrations, using golang-migrate

---
+++ cmd/migrate/migrations/20240723111310_add-user-table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE
+  IF NOT EXISTS users (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `firstName` VARCHAR(255) NOT NULL,
+    `lastName` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email` (`email`)
+  )
